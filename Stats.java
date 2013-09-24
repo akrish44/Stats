667e74ad603c728b66b37e5767c81c99b2c9bf01
Susmita <akrish44@aol.com>
2013-09-23 19:34:30 -0600

    Figured out the median of a set of data

--- Stats.java
+++ Stats.java
@@ -2,11 +2,14 @@ public class Stats {
 	public static void main(String[] args) {
 		
 
-		int[] a = { -8, 2, -10, 0};
+		// int[] a = { -8, 2, -10, 0};
+		int[] b = {-1, 0, 2, 4, 7, 8};
 
 		// System.out.println("The maximum is " + findMax(a));
 		// System.out.println("The minimum is " + findMin(a));
-		System.out.println("The mean is " + findMean(a));
+		// System.out.println("The mean is " + findMean(a));
+		System.out.println("The median is " + findMedian(b));
+		// System.out.println("The first quartile is " + findQuart1(b));
 	}
 
 
@@ -31,7 +34,7 @@ public class Stats {
 		return min;
 	}
 	
-	public static double findMean(int[] a) {
+	public static double findMean(int[] a, int n) {
 		double mean =0;
 		int sum = 0;
 		for (int i=0; i<a.length; i++) {
@@ -41,5 +44,44 @@ public class Stats {
 		mean = sum/a.length;
 		return mean;
 	}
-	
+
+	public static double findMedian(int[] b) {
+		double median = 0;
+		int medianStart = b.length/2;
+
+		for (int i=0; i<b.length; i++) {
+			
+		
+		if (b.length % 2 ==0) {
+			median = (b[medianStart-1] + b[medianStart]) / 2.0;
+		} else {
+			median = b[medianStart];
+		}
+	}
+		return median;
+
+} 
+
+	// public static double findQuart1(int[] b) {
+	// 	for (int i=0; i<b.length; i++) {
+	// 		if (b.length%2==0) {
+	// 			quart1Start = b[(b.length/2)];
+	// 			quart1 = 
+	// 		}
+			
+	// 	}
+	// }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
