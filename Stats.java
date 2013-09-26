28cc61cf47d1b4292117ae9aa8a4d16b9cc499f4
akrish44 <akrish44@aol.com>
2013-09-25 21:05:21 -0600

    Update Stats.java

--- Stats.java
+++ Stats.java
@@ -2,21 +2,22 @@ public class Stats {
 	public static void main(String[] args) {
 
 
-		int[] a = { -8, 2, -10, 0};
-		// int[] b = {-2, 1, 3, 4, 5, 6, 7};
-		// int[] c = {2, 4, -1, 0, 2, 2, 0, 8, 2, 0, 2};
 
-		// System.out.println("The maximum is " + max(a));
-		// System.out.println("The minimum is " + min(a));
-		// System.out.println("The mean is " + mean(a));
-		// System.out.println("The median is " + median(b));
-		System.out.println("The first quartile is " + quartile1(b));
-		// System.out.println("The third quartile is " + quartile3(b));
-		// System.out.println("The mode is " + mode(c));
-		// System.out.println("The standard deviation is " + standardDeviation(a));
+
+		int[] a = {-12, -12, 3, 8, 10, 11, 12, 13, 15};
+
+		// System.out.println(max(a));
+		// System.out.println(min(a));
+		// System.out.println(mean(a));
+		// System.out.println(median(a));
+		// System.out.println(quartile1(a));
+		// System.out.println(quartile3(a));
+		// System.out.println(mode(a));
+		System.out.println(standardDeviation(a));
+
 	}
 
-
+	
 	public static int max(int[] a) {
 		int max = a[0]; 
 		for (int i=0; i<a.length; i++) {
@@ -30,7 +31,8 @@ public class Stats {
 
 	public static int min(int[] a) {
 		int min = a[0];
-		for (int i=0; i<a.length; i++) {
+
+	for (int i=0; i<a.length; i++) {
 			if (a[i]<min) {
 				min=a[i];
 			}
@@ -38,118 +40,97 @@ public class Stats {
 		return min;
 	}
 
-	public static double mean(int[] a, int n) {
+	public static double mean(int[] a) {
 		double mean =0;
 		int sum = 0;
 		for (int i=0; i<a.length; i++) {
 			sum =sum + a[i];
-			// mean = sum/a.length;
+			mean = sum/(a.length);
 		}
-		mean = sum/a.length;
 		return mean;
 	}
 
-	public static double median(int[] b) {
+
+	public static double median(int[] a) {
 		double median = 0;
-		int medianStart = b.length/2;
-
-		for (int i=0; i<b.length; i++) {
-
-
-		if (b.length % 2 ==0) {
-			median = (b[medianStart-1] + b[medianStart]) / 2.0;
+		int mid = a.length/2;
+		if (a.length % 2 ==0) {
+			median = (a[mid-1] + a[mid]) / 2.0;
 		} else {
-			median = b[medianStart];
+			median = a[mid];
 		}
-	}
 		return median;
 
-} 
+	}
 
-	public static double quartile1(int[] b) {
-
-		double length = b.length;
+	public static double quartile1(int[] a) {
+		double length = a.length;
 		double quarter = length/4;
 		int placement = (int)quarter;
-		double number = 0.0;
-		double sum = b[placement] + b[placement-1];
-		if (length % 4 !=0) {
-			number = b[placement];
-		} else {
-			number = sum/2;
+		double quart2 = (length-1)/4;
+		int placement2 = (int)quart2;
+		double quart1=0.0;
+		double sum = a[placement] + a[placement-1];
+		if (length%4 !=0 &&((length-1)%4)==0) {
+			quart1 = a[placement2];
 		}
-
-		return number;
+		else if (length % 4 !=0) {
+			quart1 = a[placement];
+		} else if(length % 4 !=0 && ((length-1)%4)!=0){
+			quart1 = a[placement-1];
+			
+		} else {
+			quart1 = sum/2;
+		}
+		return quart1;
 	}
 
-	public static double quart3(int[] b) {
-		int quart3 = ((b.length/4)*3);
-
-		int meanStart = b[quart3] + b[quart3 - 1];
-		int placement = (int)quart3;
-		int sum = b[placement] + b[placement - 1];
-
-		int finalQuart = 0;
-
-		for (int i=0; i<b.length; i++) {
-
-		if((b.length%4!=0)) {	
-			finalQuart = b[placement];
+	public static double quartile3(int[] a) {
+		double length = a.length;
+		double quarter = length/4;
+		double multiply = quarter * 3;
+		int placement = (int)multiply;
+		double quart3=0.0;
+		double sum = a[placement] + a[placement-1];
+		if (length % 4 != 0) {
+			quart3 = a[placement];
 		} else {
-			finalQuart = sum/2;
+			quart3 = sum/2;
 		}
-	} 
-	return finalQuart;
+		return quart3;
 	}
 
-	public static int mode(int[] c) {
+	public static int mode(int[] a) {
 
 		int value=0;
 		int maxCount=0;
 
-    	for (int i = 0; i < c.length; i++) {
+    	for (int i = 0; i < a.length; i++) {
 
         int count = 0;
-        for (int j = 0; j < c.length; j++) {
-            if (c[j] == c[i]) count++;
+        for (int j = 0; j < a.length; j++) {
+            if (a[j] == a[i]) count++;
         }
         if (count > maxCount) {
             maxCount = count;
-            value = c[i];
-        }
+            value = a[i];
+        } 
     }
 
     	return value;
 	}
 
 	public static double standardDeviation(int[] a) {
-
 		double mean = mean(a);
 		double sum = 0;
-		double change =0;
-
+		double variance =0;
 		for (int i=0; i<a.length; i++) {
-
-			double sdStart = mean - a[i];
-
-			sum = sum * (sdStart*sdStart);
-
+			double diff = mean - a[i];
+			double square = diff * diff;
+			sum += square;
 		}
-
-		double finalSD = Math.sqrt(sum/(a.length-1));
-
-		return finalSD;
+		variance = sum/(a.length-1);
+		double sd = Math.sqrt(variance);
+		return sd;
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
